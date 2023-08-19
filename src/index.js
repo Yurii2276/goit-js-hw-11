dfbdd862118bd1272a13b616b07c8d5b6651f835
Ursx2204 <127949449+Yurii2276@users.noreply.github.com>
2023-08-20 00:27:53 +0300

    50 % ready

--- src/index.js
+++ src/index.js
@@ -1,24 +1,74 @@
-import { fetchPics } from "./sass/pixaby_api";
+import { fetchPics } from './pixaby_api';
+import { createMarkup } from './markup';
+import Notiflix from 'notiflix';
 
-fetchPics("nature")
-  .then(data => {
+const refs = {
+  form: document.getElementById(`search-form`),
+  input: document.querySelector('.input-field'),
+  loadMoreBtn: document.querySelector(`.load-more`),
+  gallery: document.querySelector(`.gallery`),
+};
+
+let page = 0;
+const limitPic = 4;
+let totalPage = 0;
+const previousTerm = { value: '' };
+
+loadMoreHide();
+
+refs.form.addEventListener(`submit`, showSetPictures);
+
+refs.loadMoreBtn.addEventListener(`click`, showSetPictures);
+
+async function showSetPictures(event) {
+  event.preventDefault();
+
+  let currentSearchTerm = refs.input.value;
+  checkNewDataImput(currentSearchTerm, previousTerm);
+
+  try {
+    const data = await fetchPics(currentSearchTerm, (page += 1), limitPic);
+
     console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    console.log(data.totalHits);
+    totalPage = Math.ceil(data.totalHits / limitPic);
+    console.log(totalPage);
+    console.log(page);
 
+    refs.gallery.insertAdjacentHTML(`beforeend`, createMarkup(data.hits));
 
+    checkTheEnd(page, totalPage);
+  } catch (error) {
+      loadMoreHide();
+      console.error(error);
+  }
+}
 
+function loadMoreShow() {
+  refs.loadMoreBtn.style.display = 'block';
+}
 
+function loadMoreHide() {
+  refs.loadMoreBtn.style.display = 'none';
+}
 
+function checkTheEnd(p, tp) {
+  if (p < tp) {
+    loadMoreShow();
+  } else {
+    loadMoreHide();
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
 
-
-
-
-
-
-
-
-
-// API_KEY = 38933529-1badfc8ed6dbb186160b3796b
+function checkNewDataImput(currentTerm, previousTerm) {
+  if (currentTerm !== previousTerm.value) {
+    refs.gallery.innerHTML = '';
+    previousTerm.value = currentTerm;
+    page = 0;
+    totalPage = 0;
+    return page, totalPage;
+  }
+}
--- src/pixaby_api.js
+++ src/pixaby_api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
 const API_KEY = "38933529-1badfc8ed6dbb186160b3796b";
 const BASE_URL = "https://pixabay.com/api/";
 
-export async function fetchPics(searchTerm) {
+export async function fetchPics(searchTerm, page, limit) {
 
   const params = {
     key: API_KEY,
@@ -11,7 +12,7 @@ export async function fetchPics(searchTerm) {
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-    per_page: 40,
+    per_page: limit,
     page: page,
   };
 
@@ -20,7 +21,13 @@ export async function fetchPics(searchTerm) {
       if (response.status !== 200) {
         throw new Error(response.statusText);
       }
-      return response.data;
+      const data = response.data;
+       if (data.hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        throw new Error('No images found'); 
+      }
+
+      return data;
     })
     .catch(error => {
       throw new Error(error.message);
